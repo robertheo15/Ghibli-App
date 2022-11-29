a6847a17fda9ec32e57646303e38dcd7c4c5866e
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-11-29 11:34:49 +0700

    Set up theme

---
+++ app/src/main/java/com/robert/ghibliapp/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.robert.ghibliapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Beige = Color(0xFFE5D9B6)
+val LightGreen = Color(0xFFA4BE7B)
+val Green = Color(0xFF5F8D4E)
+val DarkGreen = Color(0xFF285430)
