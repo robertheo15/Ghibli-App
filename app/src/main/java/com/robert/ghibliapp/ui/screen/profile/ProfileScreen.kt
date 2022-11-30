88510c172e8730a35cfa4f9fb3241af21955e274
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-11-30 19:51:39 +0700

    Implement profile

---
+++ app/src/main/java/com/robert/ghibliapp/ui/screen/profile/ProfileScreen.kt
@@ -0,0 +1,107 @@
+package com.robert.ghibliapp.ui.screen.profile
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.robert.ghibliapp.R
+import com.robert.ghibliapp.ui.theme.GhibliAppTheme
+
+@Composable
+fun ProfileScreen() {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(8.dp)
+    ) {
+        ProfileImage()
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Name",
+                modifier = Modifier.width(100.dp),
+                fontSize = 22.sp
+            )
+            Text(
+                text = "Robert",
+                modifier = Modifier
+                    .padding(10.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = 22.sp
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Email",
+                modifier = Modifier.width(100.dp),
+                fontSize = 22.sp
+            )
+            Text(
+                text = "robertheo.rt@gmail.com",
+                modifier = Modifier
+                    .padding(10.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = 22.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun ProfileImage() {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(
+            shape = CircleShape,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(300.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.profile),
+                contentDescription = "profile picture",
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GhibliAppTheme {
+        ProfileScreen()
+    }
+}
