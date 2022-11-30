f6dccf0e356bdcf72ad659ceccf1c32b12a92bf7
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-11-30 15:15:11 +0700

    Create GhibliApp.kt

---
+++ app/src/main/java/com/robert/ghibliapp/GhibliApp.kt
@@ -0,0 +1,150 @@
+package com.robert.ghibliapp
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.robert.ghibliapp.ui.navigation.NavigationItem
+import com.robert.ghibliapp.ui.navigation.Screen
+import com.robert.ghibliapp.ui.screen.detail.DetailScreen
+import com.robert.ghibliapp.ui.screen.favorite.FavoriteScreen
+import com.robert.ghibliapp.ui.screen.home.HomeScreen
+import com.robert.ghibliapp.ui.screen.profile.ProfileScreen
+import com.robert.ghibliapp.ui.theme.GhibliAppTheme
+
+@Composable
+fun GhibliApp(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    Scaffold(
+        bottomBar = {
+            if (currentRoute != Screen.DetailFilm.route) {
+                BottomBar(navController)
+            }
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) {
+                HomeScreen(
+                    navigateToDetail = { filmId ->
+                        navController.navigate(Screen.DetailFilm.createRoute(filmId))
+                    }
+                )
+            }
+            composable(
+                route = Screen.DetailFilm.route,
+                arguments = listOf(navArgument("filmId") { type = NavType.LongType })
+            ) {
+                val id = it.arguments?.getLong("filmId") ?: -1L
+                DetailScreen(
+                    filmId = id,
+                    navigateBack = {
+                        navController.navigateUp()
+                    },
+                    upPressed = {
+                    }
+                )
+            }
+            composable(Screen.Favorite.route) {
+                FavoriteScreen(
+                    navigateToDetail = { filmId ->
+                        navController.navigate(Screen.DetailFilm.createRoute(filmId))
+                    }
+                )
+            }
+            composable(Screen.Profile.route) {
+                ProfileScreen()
+            }
+        }
+    }
+}
+
+@Composable
+private fun BottomBar(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    BottomNavigation(
+        modifier = modifier
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        val navigationItems = listOf(
+            NavigationItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home,
+                screen = Screen.Home
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_favorite),
+                icon = Icons.Default.Favorite,
+                screen = Screen.Favorite
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle,
+                screen = Screen.Profile
+            )
+        )
+        BottomNavigation {
+            navigationItems.map { item ->
+                BottomNavigationItem(
+                    icon = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.title
+                        )
+                    },
+                    label = { Text(item.title) },
+                    selected = currentRoute == item.screen.route,
+                    onClick = {
+                        navController.navigate(item.screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            restoreState = true
+                            launchSingleTop = true
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GhibliAppPreview() {
+    GhibliAppTheme {
+        GhibliApp()
+    }
+}
