e634d56ae8d7b8d2a84a91e855e37a5542291529
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-11-30 20:02:02 +0700

    Update MovieDetailTopBar.kt

--- app/src/main/java/com/robert/ghibliapp/ui/navigation/MovieDetailTopBar.kt
+++ app/src/main/java/com/robert/ghibliapp/ui/navigation/MovieDetailTopBar.kt
@@ -1,11 +1,7 @@
 package com.robert.ghibliapp.ui.navigation
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
@@ -13,21 +9,17 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun MovieDetailsTopBar(
     title: String,
     isFavorite: Boolean,
     onFavoriteClicked: () -> Unit,
-    upPressed: () -> Unit
+    navigateBack: () -> Unit
 ) {
     TopAppBar(
-//        backgroundColor = androidx.compose.material3.MaterialTheme.colorScheme.background,
-//        contentColor = androidx.compose.material3.MaterialTheme.colorScheme.onBackground,
         title = {
             Text(
                 text = title,
@@ -36,7 +28,7 @@ fun MovieDetailsTopBar(
             )
         },
         navigationIcon = {
-            IconButton(onClick = upPressed) {
+            IconButton(onClick = navigateBack) {
                 Icon(
                     imageVector = Icons.Default.ArrowBack,
                     contentDescription = "Back to Home"
@@ -44,26 +36,16 @@ fun MovieDetailsTopBar(
             }
         },
         actions = {
-//            if (isFavoriteLoading) {
-//                CircularProgressIndicator(
-//                    modifier = Modifier
-//                        .padding(8.dp)
-//                        .size(24.dp),
-//                    color = MaterialTheme.colorScheme.primary,
-//                    strokeWidth = 2.dp
-//                )
-//            } else {
             IconButton(
                 onClick = onFavoriteClicked,
                 content = {
                     Icon(
                         imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
-                        contentDescription = null,
+                        contentDescription = title,
                         tint = Color.Red
                     )
                 }
             )
-//            }
         }
     )
 }
